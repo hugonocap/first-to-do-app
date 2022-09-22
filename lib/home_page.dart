02e721355be13f44d435689b9914c7780dc2efa1
Hugo Montana <benzlbenny2k@gmail.com>
2022-09-22 21:07:09 +0300

    app setup and checkbox functionality

---
+++ lib/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutt/todo_tile.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //* list of todo tasks
+  List toDoList = [
+    ['Make tutorial', false],
+    ['Make cake', false],
+  ];
+
+  //* checkbox was tapped
+  void CheckboxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow.shade200,
+      appBar: AppBar(
+        title: const Center(
+          child: Text('MAGA To Do'),
+        ),
+        elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => CheckboxChanged(value, index),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutt/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+    );
+  }
+}
