e21c3da2f3f27ce8d820bbe56b4c05280afb850b
Hugo Montana <benzlbenny2k@gmail.com>
2022-09-23 00:05:57 +0300

    database works correctly

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -4,5 +4,22 @@ class ToDoDataBase {
   List toDoList = [];
 
   //* reference the box
-  final _mybox = Hive.openBox('mybox');
+  final _myBox = Hive.box('mybox');
+
+  //* run this method if this is the first time ever of opening this app
+  void createInitialData() {
+    toDoList = [
+      ['Swipe to delete', false],
+    ];
+  }
+
+  //* load the data from database
+  void loadData() {
+    toDoList = _myBox.get('TODOLIST');
+  }
+
+  //* update the database
+  void updateDataBase() {
+    _myBox.put('TODOLIST', toDoList);
+  }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,8 +13,21 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   //*  reference the hive box
-  final _mybox = Hive.openBox('mybox');
+  final _myBox = Hive.box('mybox');
   ToDoDataBase db = ToDoDataBase();
+
+  @override
+  void initState() {
+    //* if this is the first time ever opening the app, then create default data
+    if (_myBox.get('TODOLIST') == null) {
+      db.createInitialData();
+    } else {
+      //* there already exist data
+      db.loadData();
+    }
+
+    super.initState();
+  }
 
   //* text controller
   final _controller = TextEditingController();
@@ -24,6 +37,7 @@ class _HomePageState extends State<HomePage> {
     setState(() {
       db.toDoList[index][1] = !db.toDoList[index][1];
     });
+    db.updateDataBase();
   }
 
   //* save new task
@@ -36,6 +50,7 @@ class _HomePageState extends State<HomePage> {
       _controller.clear();
     });
     Navigator.of(context).pop();
+    db.updateDataBase();
   }
 
   //* create a new task
@@ -58,6 +73,7 @@ class _HomePageState extends State<HomePage> {
     setState(() {
       db.toDoList.removeAt(index);
     });
+    db.updateDataBase();
   }
 
   @override
